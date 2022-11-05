3b31ccd4165d75b7446d878f17d1915794512da2
vikaskaliramna <vikaskaliramna.me@gmail.com>
2022-11-05 21:27:56 +0530

    Setup

---
+++ default.ts
@@ -0,0 +1,7 @@
+import LanguageConstructor from './lib/Content-Processor/Constructor.TS';
+
+const CustomConstructor = new LanguageConstructor();
+
+const Process = CustomConstructor.process(await Deno.readTextFile('Example/BootstrapV5.CSS'));
+
+console.log(Process.css);

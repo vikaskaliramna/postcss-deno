8fbcc472af428ed98c22d7284d8284b992667f5f
vikaskaliramna <vikaskaliramna.me@gmail.com>
2022-11-05 21:10:27 +0530

    Basic Setup

--- deno/lib/source_map.ts
+++
@@ -1,2 +0,0 @@
-// @deno-types="https://deno.land/x/source_map@0.6.2/source-map.d.ts"
-export * from "https://deno.land/x/source_map@0.6.2/mod.js";
--- deno/test/deps.js
+++
@@ -1,2 +0,0 @@
-export * from "https://deno.land/std@0.98.0/testing/asserts.ts";
-import "https://deno.land/std@0.98.0/node/global.ts";
